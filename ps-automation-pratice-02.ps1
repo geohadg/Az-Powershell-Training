41b64123916443968d5abfef33978526d37d6950
geohadg <152939576+geohadg@users.noreply.github.com>
2024-03-01 17:23:25 -0800

    Delete ps-automation-pratice-02.ps1

--- ps-automation-pratice-02.ps1
+++
@@ -1,4 +0,0 @@
-$action = New-ScheduledTaskAction -Execute "powershell.exe" -argument "-file C:\Users\ggftt\OneDrive\Desktop\Azure Training\first-gitcl-demo\Az-Powershell-Training\ps-automation-practice.ps1"
-$trigger = New-ScheduledTaskTrigger -Daily -at "8:38 PM"
-
-Register-ScheduledTask -action $action -trigger $trigger -TaskName "test5"
