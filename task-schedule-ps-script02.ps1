3a6dbbc3e208cac680f3e942fa2b9e78ecfb562c
Donavin Geohagan <geohadg@outlook.com>
2024-03-02 19:19:26 -0600

    automated script finally works:)

--- task-schedule-ps-script02.ps1
+++ task-schedule-ps-script02.ps1
@@ -1,4 +1,4 @@
-$action = New-ScheduledTaskAction -Execute "powershell.exe" -argument "-file C:\Users\ggftt\OneDrive\Desktop\Azure Training\first-gitcl-demo\Az-Powershell-Training\ps-automation-practice.ps1"
+$action = New-ScheduledTaskAction -Execute "powershell.exe" -Argument '-ExecutionPolicy Bypass C:\Users\geoha\Az-Powershell-Training\task-schedule-ps-script01.ps1 -RunType $true -Path C:\Users\geoha\Az-Powershell-Training\task-schedule-ps-script01.ps1'
 $trigger = New-ScheduledTaskTrigger -Daily -at "8:38 PM"
 
 Register-ScheduledTask -action $action -trigger $trigger -TaskName "test5"
