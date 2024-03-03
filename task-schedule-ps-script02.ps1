ece97c20555b39983a9a501921bcef23a80b37a1
Donavin Geohagan <geohadg@outlook.com>
2024-03-02 19:25:32 -0600

    elimating unnecessary arguments

--- task-schedule-ps-script02.ps1
+++ task-schedule-ps-script02.ps1
@@ -1,4 +1,4 @@
-$action = New-ScheduledTaskAction -Execute "powershell.exe" -Argument '-ExecutionPolicy Bypass C:\Users\geoha\Az-Powershell-Training\task-schedule-ps-script01.ps1 -RunType $true -Path C:\Users\geoha\Az-Powershell-Training\task-schedule-ps-script01.ps1'
+$action = New-ScheduledTaskAction -Execute "powershell.exe" -Argument '-ExecutionPolicy Bypass C:\Users\geoha\Az-Powershell-Training\task-schedule-ps-script01.ps1 -Path C:\Users\geoha\Az-Powershell-Training\task-schedule-ps-script01.ps1'
 $trigger = New-ScheduledTaskTrigger -Daily -at "8:38 PM"
 
 Register-ScheduledTask -action $action -trigger $trigger -TaskName "test5"
