5b1aeb5dcbb48348c34180bfe3e439f3bceb5e63
geohadg <152939576+geohadg@users.noreply.github.com>
2024-03-09 16:42:30 -0600

    Delete webrequest-and-filerun.ps1

--- webrequest-and-filerun.ps1
+++
@@ -1,5 +0,0 @@
-cd\
-cd C:\Users\geoha\Downloads
-$ProgressPreference = 'silentlycontinue'
-invoke-webrequest 'https://irltoolkit.mm.fcix.net/videolan-ftp/vlc/3.0.20/win64/vlc-3.0.20-win64.exe' -outfile 'vlcsetup.exe'
-C:\Users\geoha\Downloads\vlcsetup.exe
